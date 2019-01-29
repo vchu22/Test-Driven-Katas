8ad7451d723094b79c92038fea52b97a3d23d97a
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2019-01-29 11:53:07 -0500

    finished word wrap

--- wrap.spec.js
+++ wrap.spec.js
@@ -5,4 +5,14 @@ describe('wrap', () => {
   it('Returns empty string if empty string was provided', () => {
     expect(wrap('', 10)).to.equal('');
   });
+  it('Returns the ', () => {
+    expect(
+      wrap(
+        'Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.',
+        20
+      )
+    ).to.equal(
+      'Lorem ipsum dolor\nsit eu amet, elit na\nmagna sem amet nulla\nvel purus ac ligula.'
+    );
+  });
 });
