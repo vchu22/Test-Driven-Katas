34d09eb10077f44f209edce3848cac79ffae6059
Vanessa Chu <vchu22@users.noreply.github.com>
2019-05-20 19:04:42 -0400

    edited test spec description

--- wrap.spec.js
+++ wrap.spec.js
@@ -5,7 +5,7 @@ describe('wrap', () => {
   it('Returns empty string if empty string was provided', () => {
     expect(wrap('', 10)).to.equal('');
   });
-  it('Returns the ', () => {
+  it('Returns the correctly wrapped string', () => {
     expect(
       wrap(
         'Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.',
