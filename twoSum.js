f55eb8b48514989530f6eda6c6865dc3ebef69fd
Vanessa Chu <vchu22@users.noreply.github.com>
2019-05-20 20:06:36 -0400

    passed all tests for twoSum

--- twoSum.js
+++ twoSum.js
@@ -1,4 +1,18 @@
-const twoSum = (arr, num) => {
-    return []
+const twoSum = (arr, target) => {
+    if (arr.length < 2)
+        return []
+    let complements = {};
+  
+    for(let i=0; i<arr.length; i++) {
+        const curr = arr[i];
+        const diff = target - curr;
+        
+        if (complements[curr] !== undefined) {
+            return [complements[curr], i];
+        } else {
+            complements[diff] = i;
+        }
+    }
+    return [];
 }
 module.exports = twoSum
--- twoSum.spec.js
+++ twoSum.spec.js
@@ -3,6 +3,16 @@ const twoSum = require('./twoSum')
 
 describe('twoSum', () => {
     it('takes in an array and a target number, and returns an array', () => {
-        expect(Array.isArray(twoSum([1, 2, 3], 5))).to.equal(true)
+        expect(Array.isArray(twoSum([1, 1], 2))).to.equal(true)
+    })
+    it('returns the first index pair of numbers occurred in the array which add up to the target number in the second parameter', () => {
+        
+        expect(twoSum([1, 2, 3], 5)).to.eql([1, 2])
+    })
+    it('returns an empty array if no pairs add up to the target number', () => {
+        expect(twoSum([5, 3, 1], 2).length).to.equal(0)
+    })
+    it('returns empty array when the input array is either empty or has only one number', () => {
+        expect(twoSum([], 1).length).to.equal(0)
     })
 })
